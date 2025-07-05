b1ac9475ab7206ba43afbe45d2f96a681452f56e
Sharvin <gavad.sharvin@gmail.com>
2025-07-06 03:26:53 +0530

    Improved File Structure

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,4 +1,4 @@
-import { NOTION_TOKEN, DATABASE_ID } from "./secrets.js";
+import { NOTION_TOKEN, DATABASE_ID } from "../secrets.js";
 
 const toggleSwitch = document.getElementById('toggleSwitch');
 
